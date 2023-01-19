02266429dffcd19f24925e6a0c8625930b786205
Victoria <victoriareis_@outlook.com>
2023-01-19 13:47:47 -0500

    deleted a starter icons/logos and set up React router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ShoppingPage from "./pages/ShoppingPage";
+import ContactPage from "./pages/ContactPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>My E-Commerce</h1>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/shopping" element={<ShoppingPage />} />
+				<Route path="/cart" element={<CartPage />} />
+				<Route path="/contact" element={<ContactPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
