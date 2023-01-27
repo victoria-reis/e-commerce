dff3b94cecc64aef2d6f24ae3cc86bcb85305c49
Victoria <victoriareis_@outlook.com>
2023-01-27 12:15:27 -0500

    made home page styles responsive

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,31 +6,31 @@ import ShopWomensImage from "../assets/shop-womens.jpg";
 
 const HomePage = () => {
 	return (
-		<div className="grid grid-cols-2 gap-2">
+		<div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
 			<Link
 				to="/shopping"
 				style={{ backgroundImage: `url(${ShopAllImage})` }}
-				className="col-span-2 xl:h-[550px] h-[450px] bg-center bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
+				className="sm:col-span-2 xl:h-[550px] sm:h-[450px] h-[250px] bg-center bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
 			>
-				<h2 className="block w-max text-8xl text-white drop-shadow-lg">
+				<h2 className="block w-max text-5xl md:text-8xl text-white drop-shadow-lg">
 					Shop All
 				</h2>
 			</Link>
 			<Link
 				to="/shopping/womens"
 				style={{ backgroundImage: `url(${ShopWomensImage})` }}
-				className="h-[700px] bg-center bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
+				className="h-[450px] sm:h-[700px] sm:bg-center bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
 			>
-				<h2 className="block w-max xl:text-8xl text-6xl text-center text-white drop-shadow-lg">
+				<h2 className="block w-max xl:text-8xl text-5xl md:text-6xl text-center text-white drop-shadow-lg">
 					Shop Women's
 				</h2>
 			</Link>
 			<Link
 				to="/shopping/mens"
 				style={{ backgroundImage: `url(${ShopMensImage})` }}
-				className="h-[700px] bg-top xl-bg-top bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
+				className="h-[450px] sm:h-[700px] bg-center xl:bg-bottom bg-cover flex justify-center items-center brightness-[.85] hover:brightness-100 ease-in duration-200"
 			>
-				<h2 className="block w-max xl:text-8xl text-6xl text-center text-white drop-shadow-lg">
+				<h2 className="block w-max xl:text-8xl text-5xl md:text-6xl text-center text-white drop-shadow-lg">
 					Shop Men's
 				</h2>
 			</Link>
