d869f26c9b97e8d5624703c069d2ece0a5a35b9f
Victoria <victoriareis_@outlook.com>
2023-02-14 15:54:43 -0500

    commented unused states in order to deploy site for testing

--- src/store/UIContext.js
+++ src/store/UIContext.js
@@ -3,16 +3,13 @@ import React, { useState } from "react";
 export const UIContext = React.createContext();
 
 export const UIProvider = (props) => {
-	const [initialRender, setInitialRender] = useState(true);
 	const [isLoading, setIsLoading] = useState(false);
 	const [errorMessage, setErrorMessage] = useState(undefined);
-	const [showSidebar, setShowSidebar] = useState(false);
+	// const [showSidebar, setShowSidebar] = useState(false);
 
 	return (
 		<UIContext.Provider
 			value={{
-				initialRender,
-				setInitialRender,
 				isLoading,
 				setIsLoading,
 				errorMessage,
